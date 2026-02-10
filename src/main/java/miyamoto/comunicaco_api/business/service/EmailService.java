a8abd629ed0fe764c03b601b50ab65f3b035fa28
hkojim <eng.koji@gmail.com>
2026-02-10 12:02:49 -0300

    Inserido a dependencia do Java Mail Sender e Thymeleaf | Criado a classe EmailController para enviar  email | Criado a classe EmailService para gerar e editar o email | Criado o EmailException | Configurado o application prroperties para enviar email através do gmail | Inserido o template para enviar o emmail editado

---
+++ src/main/java/miyamoto/comunicaco_api/business/service/EmailService.java
@@ -0,0 +1,58 @@
+package miyamoto.comunicaco_api.business.service;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
+import miyamoto.comunicaco_api.api.dto.ComunicacaoOutDTO;
+import miyamoto.comunicaco_api.infraestructure.execptions.EmailException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
+
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+
+    private final JavaMailSender javaMailSender;
+
+    private final TemplateEngine templateEngine;
+
+    @Value("${envio.email.remetente}")
+    public String remetente;
+
+    @Value("{$envio.email.nomeRemetente}")
+    private String nomeRemetente;
+
+    public void enviaEmail(ComunicacaoOutDTO dto){
+        try{
+            MimeMessage mensagem = javaMailSender.createMimeMessage();
+            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mensagem, true, StandardCharsets.UTF_8.name());
+            mimeMessageHelper.setFrom(new InternetAddress(remetente, nomeRemetente));
+            mimeMessageHelper.setTo(InternetAddress.parse(dto.getEmailDestinatario()));
+            mimeMessageHelper.setSubject("Comunicação da mensagem");
+
+
+            Context context = new Context();
+            context.setVariable("mensagem", dto.getMensagem());
+            context.setVariable("dataHoraEnvio", dto.getDataHoraEnvio());
+
+            String template = templateEngine.process("notificacao", context);
+            mimeMessageHelper.setText(template, true);
+            javaMailSender.send(mensagem);
+
+        } catch (MessagingException | UnsupportedEncodingException e) {
+            throw new EmailException("Erro ao enviar o email ", e.getCause());
+        }
+    }
+
+
+
+
+}
