6e2b64a3b0e98d2142ff86bde69cbd428949422b
hkojim <eng.koji@gmail.com>
2026-02-10 15:13:40 -0300

    Adicionado no build.gradle as dependicias de mapstruct | Criado a interface ComunicacaoConverterMapper para fazer os conversores com o mapstruct | Alterado a classe ComunicacaoService para utilizar a interface ComunicacaoConverterMapper

--- src/main/java/miyamoto/comunicaco_api/api/dto/ComunicacaoInDTO.java
+++ src/main/java/miyamoto/comunicaco_api/api/dto/ComunicacaoInDTO.java
@@ -2,13 +2,11 @@ package miyamoto.comunicaco_api.api.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-
 import lombok.*;
 import miyamoto.comunicaco_api.infraestructure.enums.ModoEnvioEnum;
 import miyamoto.comunicaco_api.infraestructure.enums.StatusEnvioEnum;
 
-import java.io.Serializable;
-import java.util.Date;
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
@@ -16,10 +14,10 @@ import java.util.Date;
 @NoArgsConstructor
 @Builder
 @ToString
-public class ComunicacaoInDTO implements Serializable {
+public class ComunicacaoInDTO{
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
-    private Date dataHoraEnvio;
+    private LocalDateTime dataHoraEnvio;
     private String nomeDestinatario;
     private String emailDestinatario;
     private String telefoneDestinatario;
